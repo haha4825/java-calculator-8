494731831b914c420d0dba1e140a0315006e38a1
haha4825 <tw086219@gmail.com>
2025-10-20 11:43:46 +0900

    feat(application): extract special delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,10 +12,24 @@ public class Application {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        String[] split = input.split("[:|,]");
+        String patternString = "^(//)(.)(\\\\n)";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(input);
 
-        for (String s : split) {
-            System.out.println(s);
+        List<Character> delimiters = new ArrayList<>();
+        delimiters.add(',');
+        delimiters.add(':');
+
+        while (matcher.find()) {
+            String delimiter = matcher.group(2);
+            delimiters.add(delimiter.charAt(0));
+            input  = input.substring(matcher.end());
+            matcher = pattern.matcher(input);
         }
+
+        for (char delimiter : delimiters) {
+            System.out.println(delimiter);
+        }
+        System.out.println("계산할 문자열: " + input);
     }
 }
