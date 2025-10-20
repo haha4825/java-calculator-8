11f041d2515ca5f41140804edc341fd044bd87c4
haha4825 <tw086219@gmail.com>
2025-10-20 09:58:52 +0900

    feat(application): split string by base delimiter

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,19 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+
+        String[] split = input.split("[:|,]");
+
+        for (String s : split) {
+            System.out.println(s);
+        }
     }
 }
